f9d9088a53d178ac5cb4840ffb6f98f45ec77604
Tobias Jung <jung-tobias@outlook.de>
2020-10-18 20:45:15 +0200

    add test for caa

--- test.dns.js
+++ test.dns.js
@@ -858,3 +858,43 @@ describe("RECORD GPOS", function() {
   });
 
 });
+
+describe("RECORD CAA", function() {
+
+  let test = new DNSPacket("2a41858000010001000000010477696b690477696b690001010001c00c0101000100002a30000f8009697373756577696c64746573740000290500000000000000").printPacket();
+  
+  it("Check Header", function(){
+    expect(test.Header.id).to.equal(10817);
+    expect(test.Header.flags.qr).to.equal(1);
+    expect(test.Header.flags.opcode).to.equal("0000");
+    expect(test.Header.flags.aa).to.equal("1");
+    expect(test.Header.flags.tc).to.equal("0");
+    expect(test.Header.flags.rd).to.equal("1");
+    expect(test.Header.flags.ra).to.equal("1");
+    expect(test.Header.flags.z).to.equal("000");
+    expect(test.Header.flags.rcode).to.equal("0000");
+  });
+
+  it("Count of Sections", function(){
+    expect(test.Questions.length).to.equal(test.Header.qdcount);
+    expect(test.Answers.length).to.equal(test.Header.ancount + test.Header.nscount + test.Header.arcount);
+  });
+
+  it("Question", function(){
+    expect(test.Questions[0].type).to.equal("CAA");
+    expect(test.Questions[0].name).to.equal("wiki.wiki");
+    expect(test.Questions[0].class).to.equal("INTERNET");
+  });
+
+  it("Answer", function(){
+    expect(test.Answers[0].name).to.equal("wiki.wiki");
+    expect(test.Answers[0].type).to.equal("CAA");
+    expect(test.Answers[0].class).to.equal("INTERNET");
+    expect(test.Answers[0].ttl).to.equal(10800);
+    expect(test.Answers[0].size).to.equal(15);
+    expect(test.Answers[0].data.IssuerCritical).to.equal(true);
+    expect(test.Answers[0].data.tag).to.equal("issuewild");
+    expect(test.Answers[0].data.value).to.equal("test");
+  });
+
+});
