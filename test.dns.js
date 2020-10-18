d5c740f35ec301dfcbe42f4307a60e63bc8105b5
JUNG <tobias.jung@lhsystems.com>
2020-10-18 03:45:44 +0200

    add isdn record

--- test.dns.js
+++ test.dns.js
@@ -586,3 +586,44 @@ describe("RECORD MINFO", function() {
   });
 
 });
+
+
+
+describe("RECORD ISDN", function() {
+
+  let test = new DNSPacket("2b8a858000010001000000000477696b690477696b690000140001c00c0014000100002a3000190f303034393137363832363635393938083133303931393939").printPacket();
+  
+  it("Check Header", function(){
+    expect(test.Header.id).to.equal(11146);
+    expect(test.Header.flags.qr).to.equal(1);
+    expect(test.Header.flags.opcode).to.equal("0000");
+    expect(test.Header.flags.aa).to.equal("1");
+    expect(test.Header.flags.tc).to.equal("0");
+    expect(test.Header.flags.rd).to.equal("1");
+    expect(test.Header.flags.ra).to.equal("1");
+    expect(test.Header.flags.z).to.equal("000");
+    expect(test.Header.flags.rcode).to.equal("0000");
+  });
+
+  it("Count of Sections", function(){
+    expect(test.Questions.length).to.equal(test.Header.qdcount);
+    expect(test.Answers.length).to.equal(test.Header.ancount + test.Header.nscount + test.Header.arcount);
+  });
+
+  it("Question", function(){
+    expect(test.Questions[0].type).to.equal("ISDN");
+    expect(test.Questions[0].name).to.equal("wiki.wiki");
+    expect(test.Questions[0].class).to.equal("INTERNET");
+  });
+
+  it("Answer", function(){
+    expect(test.Answers[0].name).to.equal("wiki.wiki");
+    expect(test.Answers[0].type).to.equal("ISDN");
+    expect(test.Answers[0].class).to.equal("INTERNET");
+    expect(test.Answers[0].ttl).to.equal(10800);
+    expect(test.Answers[0].size).to.equal(25);
+    expect(test.Answers[0].data.isdnAdress).to.equal("004917682665998");
+    expect(test.Answers[0].data.subadress).to.equal("13091999");
+  });
+
+});
