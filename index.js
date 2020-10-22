c4992c7be3a402f1ba37c0757c564c55b6b8bf01
Tobias Jung <jung-tobias@outlook.de>
2020-10-22 23:09:59 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-var DNSPacket = require("./lib/DNSPacket");
-var DNSQuestion = require("./lib/DNSQuestion");
 var DNSAnswer = require("./lib/DNSAnswer");
-var DNSHeader = require("./lib/DNSHeader");
 var DNSServer = require("./lib/DNSServer");
 var RR = require("./lib/record");
 
@@ -13,12 +10,12 @@ server.listen({ port: 53, host: "0.0.0.0" }, function(message, remote, srv){
         new DNSAnswer("google.com", "INTERNET", 123,
         new RR.A("1.2.3.4"))
     );
-
+    
     message.addAnswerRR(
         new DNSAnswer("google.com", "INTERNET", 123,
         new RR.DNAME("www.google.com"))
     );
-
+    
     server.answer(message, remote);
 
 });
